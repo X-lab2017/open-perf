e9a23aae3d01a477db0d9410b346fcc41ec56d15
mali8-wps <maligg0529@gmail.com>
2024-06-22 11:38:38 +0800

    【Project:Data pre-processing】【Bug Id:0】Process the issue-gen-nextjs-question-gen folder data 【Reason: Developmental requirement 】 【Change:Developmental requirement 】 【Impact:Whether the data meets expectations】

---
+++ openperf/benchmarks/data_science/osatg-gpt/issue-gen-nextjs-question-gen/handler.py
@@ -0,0 +1,30 @@
+import json
+
+with open('./meta-sample-data.json', 'r', encoding='utf-8') as file:
+	data = json.load(file)
+
+output_lines = []
+
+for item in data:
+	question = item.get("title", "")
+	closed_answer = item.get("closed_answer", "")
+	reaction_answer = item.get("reaction_answer", "")
+
+	answer = []
+	if closed_answer:
+		answer.append(closed_answer)
+	if reaction_answer:
+		answer.append(reaction_answer)
+
+	if question and answer:
+		output_item = {
+			"sentences": answer,
+			"question": question
+		}
+		output_lines.append(json.dumps(output_item, ensure_ascii=False))
+
+with open('fine-turning-sample-data.json', 'w', encoding='utf-8') as file:
+	for line in output_lines:
+		file.write(line + '\n')
+
+print("JSON data has been written")
