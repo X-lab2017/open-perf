2f07347e45c28b58782951086f90c34c23355bcc
mali8-wps <maligg0529@gmail.com>
2024-06-23 16:56:34 +0800

    【Project:Data pre-processing】【Bug Id:0】Fixed the PR-cls-nlbse24-comment-sentiment-cls folder data 【Reason: default value from instance_type , instance_type : 0 - negetive, 1-positive】 【Change:Developmental requirement 】 【Impact:Whether the data meets expectations】

--- openperf/benchmarks/data_science/osatg-gpt/PR-cls-nlbse24-comment-sentiment-cls/handler.py
+++ openperf/benchmarks/data_science/osatg-gpt/PR-cls-nlbse24-comment-sentiment-cls/handler.py
@@ -6,13 +6,22 @@ json_file = 'fine-turning-sample-data.json'
 
 json_data = []
 
+def convert_label(instance_type):
+	if instance_type == '0':
+		return 'negative'
+	elif instance_type == '1':
+		return 'positive'
+	else:
+		return instance_type
+
 with open(csv_file, mode='r', encoding='utf-8') as file:
 	reader = csv.DictReader(file)
 	for row in reader:
+		converted_label = convert_label(row['instance_type'])
 		json_entry = {
-		"sentences": row['comment_sentence'],
+			"sentences": row['comment_sentence'],
 			"labels": {
-				"default": [row['category']]
+				"default": [converted_label]
 			}
 		}
 
@@ -23,4 +32,4 @@ with open(json_file, mode='w', encoding='utf-8') as file:
 		json_line = json.dumps(entry, ensure_ascii=False, separators=(',', ':'))
 		file.write(json_line + '\n')
 
-print(f"JSON data has been written {json_file}")
+print(f"JSON data has been written to {json_file}")
